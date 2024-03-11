6023f8614578829d901b9effbbadc4de5b6bf5ed
AntonSkyrda <mr.skyrda@gmail.com>
2024-03-11 10:55:36 +0200

    register app in settings.py, create models and register them in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ("tags",)
+
+
+admin.site.register(Tag)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+    date = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.title}, {self.done}, {self.date}, {self.tags}, {self.description}"
